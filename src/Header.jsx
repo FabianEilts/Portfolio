50558a6df6eff73c07003acd34ff58cd734db542
Fabian Eilts <fabianeilts17@gmail.com>
2023-10-22 16:31:54 +0200

    init

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+    return (
+            <header>
+                <nav>
+                    <p>Hallo</p>
+                </nav>
+            </header>
+    )
+}
+
+export default App;
---
+++ src/Sections.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const Neu = () => {
+    return (
+    <main>
+        <h1>Jaja</h1>
+    </main>
+)
+}
+
+export default Neu;
