74135ed900db2ca2affbfe9eeff458f817967a90
Fabian Eilts <fabianeilts17@gmail.com>
2023-12-23 15:47:33 +0100

    DEV: added AboutSection

--- src/Sections.jsx
+++ src/Sections.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import HomeSection from './sections/HomeSection.jsx'
+import AboutSection from "./sections/AboutSection.jsx";
 
 const Sections = () => {
     return (
     <main className="mx-40">
         <HomeSection />
+        <AboutSection />
     </main>
 )
 }
