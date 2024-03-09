b5852da6cf4ec5c6921b999569cbdf573d8baee0
Fabian Eilts <fabianeilts17@gmail.com>
2024-03-09 22:12:45 +0100

    CHANGE: header stay fixed on scroll

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,21 @@
 import './App.css'
+import "./styles/Header.css"
+import HireMeBtn from './components/buttons/HireMeBtn'
 
 const Header = () => {
+    const scrolled = () => {
+        window.addEventListener('scroll', function () {
+            let header = document.getElementById('header');
+            if(window.scrollY > 50){
+                header.classList.add('scrolled');
+            }else{
+                header.classList.remove('scrolled');
+            }
+        });
+    }
     return (
-            <header>
+        <header id='header' className='header fixed w-full top-0'>
+                <script>{scrolled()}</script>
                 <nav className='flex justify-between mx-12 my-5'>
                     <div id="logo">
                     <svg width="58" height="58" viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -16,11 +29,12 @@ const Header = () => {
                         <rect y="25" width="8" height="8" rx="4" fill="#E9B171"/>
                     </svg>
                     </div>
-                    <div className='flex gap-10 place-items-center'>
-                        <a href=""><span className="text-secondaryColor glow-effect">01. </span>About</a>
-                        <a href=""><span className="text-secondaryColor glow-effect">02. </span>Skills</a>
-                        <a href=""><span className="text-secondaryColor glow-effect">03. </span>Projects</a>
-                        <a href=""><span className="text-secondaryColor glow-effect">04. </span>Contact</a>
+                    <div className='flex gap-14 place-items-center mr-16'>
+                        <a className='text-xl' href="#">About</a>
+                        <a className='text-xl' href="#">Skills</a>
+                        <a className='text-xl' href="#">Projects</a>
+                        <a className='text-xl' href="#">Contact</a>
+                        <HireMeBtn />
                     </div>
                 </nav>
             </header>
